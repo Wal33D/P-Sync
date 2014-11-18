f3f613e69cb04014531c4f2b62ab038ca7049163
WAL33D <aquataze@yahoo.com>
2014-11-18 17:36:28 -0500

    .

--- pSync.c
+++ pSync.c
@@ -36,31 +36,47 @@
 #define handle_error_en(en, msg) \
         do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
-long circleCount = 0;	
-pthread_t *threads;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+long circleCount = 0;	/* Global Circle Count */
+pthread_t *threads;		/* Thread Identifier Decleration */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; /* Mutex init */
+
+/* 
+   *monteCarloPi() is our pthread_create entry point, It calculates 
+   an approximation of Pi using the "Monte Carlo" Method. It creates
+   a specified # of random points and determines the number of points
+   that fall within an inner circle, and calculates pi using, 
+   i) (circle_area) / (square_area) = approx.. 
+   ii) For r = 1, (circle_area) / (square_area) = π r 2 / (2r) 2 = π /4.
+*/
 
 void *monteCarloPi(void *thread_ID) {
 
-	long i;
-	int a = (int) thread_ID;
-	long incircle_thread = 0;
-	unsigned int rand_state = rand();
-
-	for (i = 0; i < NR_PTRS_PER_THREAD; i++) {
+	long i;							/* Counter */
+	int a = (int) thread_ID;		/* This threads # */
+	long incircle_thread = 0; 		/* # of points in this threads circle */
+	unsigned int rand_state = rand(); /* Random Generator */
 	
+	/* Create NR_PTRS_PER_THREAD # of points */
+	for (i = 0; i < NR_PTRS_PER_THREAD; i++) {
+		
 		double x = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
 		double y = rand_r(&rand_state) / ((double)RAND_MAX + 1) * 2.0 - 1.0;
-	
+		
+		/* If current point is within the circle */
 		if (x * x + y * y < 1) {
-
+		
+			/* Increase # incircle count */
 			incircle_thread++;
 			
 			}
 
 		}
 
-	float Pi = (4. * (double)incircle_thread) / ((double)NR_PTRS_PER_THREAD * 1);
+	/* calculate this threads approximation of Pi */
+	float Pi = (4. * (double)incircle_thread) / 
+				((double)NR_PTRS_PER_THREAD * 1);
+
+	/* Report to the console, this threads calculation of Pi */
 	printf("Thread [%d] Reports Pi to be : %f\n" ,a,Pi);
 	
 	/* Preventing against race conditions on Global circleCount 
@@ -75,22 +91,25 @@ void *monteCarloPi(void *thread_ID) {
 
 		pthread_mutex_unlock(&mutex);
 
+return NULL;
 }
 
+/*
+   Createthreads() creates NR_THREADS # of threads,allocates 
+   space for each one.If there is no pthread_create error
+*/
 void createThreads(){
 	
-	int i, s;
-	
-	int *a = malloc(sizeof(*a));
+	int i, s; /* Declare Count and ErrorNumber variables */
 
-	threads = malloc(NR_THREADS * sizeof(pthread_t));
+	threads = malloc(NR_THREADS * sizeof(pthread_t)); /* Allocate space for next thread */
 
-	pthread_attr_t attr;
+	pthread_attr_t attr; /* Pthread attribute init */
 		
 	pthread_attr_init(&attr);
 	
 	printf("\n----------------------------------------\n*Creating [%d] Threads\n\n", NR_THREADS);
-	
+	/* Create 1 thread for each NR_THREADS */
 	for (i = 0; i < NR_THREADS; i++) {
 		
 		s = pthread_create(&threads[i], &attr, monteCarloPi,  (void *) i);
@@ -104,10 +123,15 @@ void createThreads(){
 
 }
 
+/*
+   joinThreads() Joins NR_THREADS # of threads, frees
+   space for each one. If there is no pthread_join error
+*/
 void joinThreads(){
 
-	int i,s;
-	
+	int i,s; /* Declare Count and Error # Variable
+		
+	/* Join 1 thread for each NR_THREADS */
 	for (i = 0; i < NR_THREADS; i++) {
 		
 		s = pthread_join(threads[i], NULL);
@@ -117,27 +141,35 @@ void joinThreads(){
 			handle_error_en(s, "pthread_join");
 		 
 		 }
+
 	}
-	
+
+	/* Destroy mutex */
 	pthread_mutex_destroy(&mutex);
 	
+	/* Prompt user, all threads joined */
 	printf("\n*[%d] Threads Rejoined\n\n", NR_THREADS);
-
+	
+	/* Free space used by threads */
 	free(threads);
 }
-/* Calculate Pi by the Monte Carlo method. Program arguments are the total number of random
- * points to use in the calculation and the number of threads to use. 
- */
-int main(int argc, const char *argv[]) {
+/* main() acts as the controller, first calling createThreads
+   to create our 5 threads, then calling joinThreads to join each thread
+   and finally, on return, calculating Pi using the 5 threads results
+*/
 
-	float Pi;
+int main() {
 
-	createThreads();
+	float Pi; /* Declare float for final Pi calculation */
 
-	joinThreads();
+	createThreads(); /* Create threads */
+
+	joinThreads(); /* Join threads */
 	
+	/*Calculate Pi using Monte Carlo method and Global incircle count*/
 	Pi = (4. * (double)circleCount) / ((double)NR_PTRS_PER_THREAD * NR_THREADS);
-
+	
+	/* Prompt user with final approximation of Pi */
 	printf("Final Estimation of Pi: %f\n\n----------------------------------------\n", Pi);
 	
 return 0;
